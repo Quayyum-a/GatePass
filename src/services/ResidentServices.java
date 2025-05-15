3f4a209ca496a195b9f7eb7999718132e1eb783f
Quayyum-a <quayyumariyo@gmail.com>
2025-05-15 11:09:34 +0100

    add login method to ResidentServices interface for handling resident login requests

--- src/services/ResidentServices.java
+++ src/services/ResidentServices.java
@@ -1,8 +1,12 @@
 package services;
 
+import dtos.request.LoginResidentRequest;
 import dtos.request.RegisterResidentRequest;
+import dtos.response.LoginResidentResponse;
 import dtos.response.RegisterResidentResponse;
 
 public interface ResidentServices {
     RegisterResidentResponse register(RegisterResidentRequest request);
+    LoginResidentResponse login(LoginResidentRequest request);
 }
+
