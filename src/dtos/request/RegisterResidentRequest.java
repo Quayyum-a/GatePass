212e68fd9162cfa843ad145d68f19c947f9e40e4
Quayyum-a <quayyumariyo@gmail.com>
2025-05-15 08:21:18 +0100

    add RegisterResidentRequest DTO for resident registration

---
+++ src/dtos/request/RegisterResidentRequest.java
@@ -0,0 +1,41 @@
+package dtos.request;
+
+public class RegisterResidentRequest {
+    private String fullName;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
