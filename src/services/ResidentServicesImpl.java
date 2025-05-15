9c4bd44c9df791c1813cb514e8320e23749e8d9d
Quayyum-a <quayyumariyo@gmail.com>
2025-05-15 11:09:49 +0100

    add login method to ResidentServices interface for handling resident login requests

--- src/services/ResidentServicesImpl.java
+++ src/services/ResidentServicesImpl.java
@@ -3,10 +3,12 @@ package services;
 import data.model.Resident;
 import data.repository.ResidentRepository;
 import data.repository.Residents;
+import dtos.request.LoginResidentRequest;
 import dtos.request.RegisterResidentRequest;
+import dtos.response.LoginResidentResponse;
 import dtos.response.RegisterResidentResponse;
 
-import static utils.Mapper.map;
+import static utils.Mapper.*;
 
 public class ResidentServicesImpl implements ResidentServices {
     private ResidentRepository residentRepository = new Residents();
@@ -16,5 +18,14 @@ public class ResidentServicesImpl implements ResidentServices {
     public RegisterResidentResponse register(RegisterResidentRequest request) {
         Resident savedResident = residentRepository.save(map(request));
         return map(savedResident);
+    }
+
+    @Override
+    public LoginResidentResponse login(LoginResidentRequest request) {
+        Resident resident = residentRepository.findByEmail(request.getEmail());
+        if (resident == null || !resident.getPassword().equals(request.getPassword())) {
+            return null;
+        }
+        return mapToLoginResponse(resident);
     }
 }
