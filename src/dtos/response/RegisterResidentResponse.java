25654d4f00fc1917e10ff4b5ec80491f3d78dcfb
Quayyum-a <quayyumariyo@gmail.com>
2025-05-15 09:24:41 +0100

    add RegisterResidentResponse DTO for resident registration response

---
+++ src/dtos/response/RegisterResidentResponse.java
@@ -0,0 +1,49 @@
+package dtos.response;
+
+public class RegisterResidentResponse {
+    private int id;
+    private String fullName;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
