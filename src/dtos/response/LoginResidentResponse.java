10c4474fc8e9e42548cd9e5795b9504818f24cff
Quayyum-a <quayyumariyo@gmail.com>
2025-05-15 10:57:32 +0100

    add LoginResidentRequest class for handling resident login requests

---
+++ src/dtos/response/LoginResidentResponse.java
@@ -0,0 +1,49 @@
+package dtos.response;
+
+public class LoginResidentResponse {
+    private int id;
+    private String fullName;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
