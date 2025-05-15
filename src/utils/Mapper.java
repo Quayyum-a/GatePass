ba96c9de133c28455e84a7d8eafd1bc0f43f6e2e
Quayyum-a <quayyumariyo@gmail.com>
2025-05-15 11:00:08 +0100

    add mapping methods for LoginResidentRequest and LoginResidentResponse in Mapper class

--- src/utils/Mapper.java
+++ src/utils/Mapper.java
@@ -1,7 +1,9 @@
 package utils;
 
 import data.model.Resident;
+import dtos.request.LoginResidentRequest;
 import dtos.request.RegisterResidentRequest;
+import dtos.response.LoginResidentResponse;
 import dtos.response.RegisterResidentResponse;
 
 public class Mapper {
@@ -11,10 +13,21 @@ public class Mapper {
         resident.setAddress(request.getAddress());
         resident.setPhoneNumber(request.getPhoneNumber());
         resident.setEmail(request.getEmail());
+        resident.setPassword(request.getPassword());
         return resident;
     }
     public static RegisterResidentResponse map(Resident resident) {
         RegisterResidentResponse response = new RegisterResidentResponse();
+        response.setId(resident.getId());
+        response.setFullName(resident.getFullName());
+        response.setAddress(resident.getAddress());
+        response.setPhoneNumber(resident.getPhoneNumber());
+        response.setEmail(resident.getEmail());
+        response.setPassword(resident.getPassword());
+        return response;
+    }
+    public static LoginResidentResponse mapToLoginResponse(Resident resident) {
+        LoginResidentResponse response = new LoginResidentResponse();
         response.setId(resident.getId());
         response.setFullName(resident.getFullName());
         response.setAddress(resident.getAddress());
