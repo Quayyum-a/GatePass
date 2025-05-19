907f53ca3580b0672201b8cb44e82d3e1102889b
Quayyum-a <quayyumariyo@gmail.com>
2025-05-19 21:29:09 +0100

    commit

---
+++ src/data/model/Visitor.java
@@ -0,0 +1,32 @@
+package data.model;
+
+public class Visitor {
+    private int id;
+    private String fullName;
+    private String phoneNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
