9aed59f15fc18393993bf83a56f790454ea861c6
Quayyum-a <quayyumariyo@gmail.com>
2025-05-20 07:07:36 +0100

    Implement access token management and enhance resident registration process

---
+++ src/data/model/AccessToken.java
@@ -0,0 +1,105 @@
+package data.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class AccessToken {
+    private int id;
+    private String token;
+    private LocalDateTime creationDate;
+    private LocalDateTime expiryDate;
+    private boolean isUsed;
+    private int residentId;
+
+    public AccessToken() {
+        this.token = generateToken();
+        this.creationDate = LocalDateTime.now();
+        this.expiryDate = creationDate.plusHours(24);
+        this.isUsed = false;
+    }
+
+    private String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    public boolean isValid() {
+        return !isUsed && LocalDateTime.now().isBefore(expiryDate);
+    }
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expiryDate);
+    }
+
+    public void markAsUsed() {
+        this.isUsed = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public boolean isUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(boolean used) {
+        isUsed = used;
+    }
+
+    public int getResidentId() {
+        return residentId;
+    }
+
+    public void setResidentId(int residentId) {
+        this.residentId = residentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o instanceof AccessToken comparedToken) {
+            return this.id == comparedToken.getId();
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "AccessToken{" +
+                "id=" + id +
+                ", token='" + token + '\'' +
+                ", creationDate=" + creationDate +
+                ", expiryDate=" + expiryDate +
+                ", isUsed=" + isUsed +
+                ", residentId=" + residentId +
+                '}';
+    }
+}
