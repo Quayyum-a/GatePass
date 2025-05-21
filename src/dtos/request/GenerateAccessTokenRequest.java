f7db51ac5eb29f85295008e2c4dafcfb16fe0465
Quayyum-a <quayyumariyo@gmail.com>
2025-05-21 13:20:19 +0100

    Add Security and Access Token management classes with validation and response handling

---
+++ src/dtos/request/GenerateAccessTokenRequest.java
@@ -0,0 +1,31 @@
+package dtos.request;
+
+public class GenerateAccessTokenRequest {
+    private int residentId;
+    private String visitorName;
+    private String visitorPhoneNumber;
+
+    public int getResidentId() {
+        return residentId;
+    }
+
+    public void setResidentId(int residentId) {
+        this.residentId = residentId;
+    }
+
+    public String getVisitorName() {
+        return visitorName;
+    }
+
+    public void setVisitorName(String visitorName) {
+        this.visitorName = visitorName;
+    }
+
+    public String getVisitorPhoneNumber() {
+        return visitorPhoneNumber;
+    }
+
+    public void setVisitorPhoneNumber(String visitorPhoneNumber) {
+        this.visitorPhoneNumber = visitorPhoneNumber;
+    }
+}
