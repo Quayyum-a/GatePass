afbc5a8d3868703884e38068af7ed3946776ef21
Quayyum-a <quayyumariyo@gmail.com>
2025-05-21 10:54:10 +0100

    Add method to generate access token for residents

--- src/services/ResidentServices.java
+++ src/services/ResidentServices.java
@@ -1,12 +1,14 @@
 package services;
 
+import dtos.request.GenerateAccessTokenRequest;
 import dtos.request.LoginResidentRequest;
 import dtos.request.RegisterResidentRequest;
+import dtos.response.GenerateAccessTokenResponse;
 import dtos.response.LoginResidentResponse;
 import dtos.response.RegisterResidentResponse;
 
 public interface ResidentServices {
     RegisterResidentResponse register(RegisterResidentRequest request);
     LoginResidentResponse login(LoginResidentRequest request);
+    GenerateAccessTokenResponse generateAccessToken(GenerateAccessTokenRequest request);
 }
-
