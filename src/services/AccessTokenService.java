109b526b020ed66535e8e544fcbeff2150c95c1a
Quayyum-a <quayyumariyo@gmail.com>
2025-05-21 10:53:19 +0100

    Add method to generate access token for visitors with name and phone number

--- src/services/AccessTokenService.java
+++ src/services/AccessTokenService.java
@@ -4,6 +4,7 @@ import data.model.AccessToken;
 
 public interface AccessTokenService {
     AccessToken generateToken(int residentId);
+    AccessToken generateTokenForVisitor(int residentId, String visitorName, String visitorPhoneNumber);
     boolean validateToken(String token);
     void markTokenAsUsed(String token);
     boolean isTokenExpired(String token);
