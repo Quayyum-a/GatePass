2105e5de3d19a3f559d99feb475112ad542ecc08
Quayyum-a <quayyumariyo@gmail.com>
2025-05-28 15:08:48 +0100

    Remove unnecessary comments from SecurityServiceImpl methods

--- src/services/SecurityServiceImpl.java
+++ src/services/SecurityServiceImpl.java
@@ -13,27 +13,23 @@ public class SecurityServiceImpl implements SecurityService {
 
     @Override
     public GenerateAccessTokenResponse validateVisitorAccess(String token) {
-        // Validate the token
         if (!accessTokenService.validateToken(token)) {
             throw new IllegalArgumentException("Invalid token");
         }
 
-        // Get the token details
         AccessToken accessToken = accessTokenRepository.findByToken(token);
 
-        // Map and return the response
         return mapToAccessTokenResponse(accessToken);
     }
 
     @Override
     public void grantAccess(String token) {
-        // Mark the token as used
+
         accessTokenService.markTokenAsUsed(token);
     }
 
     @Override
     public AccessToken getAccessTokenDetails(String token) {
-        // Get the token details
         AccessToken accessToken = accessTokenRepository.findByToken(token);
         if (accessToken == null) {
             throw new IllegalArgumentException("Token not found");
